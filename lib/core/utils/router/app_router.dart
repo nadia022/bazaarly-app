1dcb79b3a29fdb55364d690f68b106be154bf28b
Nadia <nadiaashraff220@gmail.com>
2026-06-23 17:33:24 +0300

    chore: implement app routing and add splash view

--- lib/core/utils/router/app_router.dart
+++ lib/core/utils/router/app_router.dart
@@ -1,51 +1,46 @@
-// import 'package:flutter/material.dart';
-// import 'package:go_router/go_router.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:bazarly_app/features/splash/presentation/views/splash_view.dart';
+import 'routes_name.dart';
 
-// import 'package:bazarly_app/features/auth/presentation/views/login_view.dart';
-// import 'package:bazarly_app/features/auth/presentation/views/sign_up_view.dart';
-// import 'package:bazarly_app/features/home/presentation/views/home_view.dart';
-// import 'package:bazarly_app/features/splash/presentation/views/splash_view.dart';
+class AppRouter {
+  AppRouter._();
 
-// import 'routes_name.dart';
+  static final GlobalKey<NavigatorState> navigatorKey =
+      GlobalKey<NavigatorState>();
 
-// class AppRouter {
-//   AppRouter._();
+  static final GoRouter router = GoRouter(
+    navigatorKey: navigatorKey,
+    initialLocation: RoutesName.splash,
 
-//   static final GlobalKey<NavigatorState> navigatorKey =
-//       GlobalKey<NavigatorState>();
+    routes: [
+      /// Splash
+      GoRoute(
+        path: RoutesName.splash,
+        name: RoutesName.splash,
+        builder: (context, state) => const SplashView(),
+      ),
 
-//   static final GoRouter router = GoRouter(
-//     navigatorKey: navigatorKey,
-//     initialLocation: RoutesName.splash,
+      // /// Login
+      // GoRoute(
+      //   path: RoutesName.login,
+      //   name: RoutesName.login,
+      //   builder: (context, state) => const LoginView(),
+      // ),
 
-//     routes: [
-//       /// Splash
-//       GoRoute(
-//         path: RoutesName.splash,
-//         name: RoutesName.splash,
-//         builder: (context, state) => const SplashView(),
-//       ),
+      // /// Sign Up
+      // GoRoute(
+      //   path: RoutesName.signUp,
+      //   name: RoutesName.signUp,
+      //   builder: (context, state) => const SignUpView(),
+      // ),
 
-//       /// Login
-//       GoRoute(
-//         path: RoutesName.login,
-//         name: RoutesName.login,
-//         builder: (context, state) => const LoginView(),
-//       ),
-
-//       /// Sign Up
-//       GoRoute(
-//         path: RoutesName.signUp,
-//         name: RoutesName.signUp,
-//         builder: (context, state) => const SignUpView(),
-//       ),
-
-//       /// Home
-//       GoRoute(
-//         path: RoutesName.home,
-//         name: RoutesName.home,
-//         builder: (context, state) => const HomeView(),
-//       ),
-//     ],
-//   );
-// }
+      // /// Home
+      // GoRoute(
+      //   path: RoutesName.home,
+      //   name: RoutesName.home,
+      //   builder: (context, state) => const HomeView(),
+      // ),
+    ],
+  );
+}
