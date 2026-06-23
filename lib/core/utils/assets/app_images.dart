966d92eeb863d6ad4c2a8021b24e6769517e5aec
Nadia <nadiaashraff220@gmail.com>
2026-06-24 01:45:51 +0300

    chore:add Bazaarly branding assets and update pubspecmanagement of image assets.

--- lib/core/utils/assets/app_images.dart
+++ lib/core/utils/assets/app_images.dart
@@ -5,5 +5,13 @@ class AppImages {
   static const String imagesSuccessDialog = 'assets/images/success_dialog.svg';
   static const String imagesFailureDialog = 'assets/images/failure_dialog.svg';
   static const String imagesSuccessSubmit = 'assets/images/success_submit.svg';
+  static const String imagesBazaarlyLogo = 'assets/images/Bazaarly_logo.svg';
+  static const String imagesBazaarlySentence = 'assets/images/Bazaarly_sentence.svg';
+  static const String imagesBazaarlyLogoPNG = 'assets/images/Bazaarly_logo.png';
+  static const String imagesBazaarlySentencePNG = 'assets/images/Bazaarly_sentence.png';
+  static const String imagesSplashScreen = 'assets/images/Splash Screen.svg';
+  static const String imagesSplashScreenPNG = 'assets/images/Splash Screen.png';
+
+
   
 }
