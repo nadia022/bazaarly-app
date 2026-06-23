be75355ff56978fd0201d26bfc7d832ba1957b6e
Nadia <nadiaashraff220@gmail.com>
2026-06-23 04:30:52 +0300

    chore: add AppRouter and RoutesName for improved navigation structure

---
+++ lib/core/utils/router/app_router.dart
@@ -0,0 +1,51 @@
+// import 'package:flutter/material.dart';
+// import 'package:go_router/go_router.dart';
+
+// import 'package:bazarly_app/features/auth/presentation/views/login_view.dart';
+// import 'package:bazarly_app/features/auth/presentation/views/sign_up_view.dart';
+// import 'package:bazarly_app/features/home/presentation/views/home_view.dart';
+// import 'package:bazarly_app/features/splash/presentation/views/splash_view.dart';
+
+// import 'routes_name.dart';
+
+// class AppRouter {
+//   AppRouter._();
+
+//   static final GlobalKey<NavigatorState> navigatorKey =
+//       GlobalKey<NavigatorState>();
+
+//   static final GoRouter router = GoRouter(
+//     navigatorKey: navigatorKey,
+//     initialLocation: RoutesName.splash,
+
+//     routes: [
+//       /// Splash
+//       GoRoute(
+//         path: RoutesName.splash,
+//         name: RoutesName.splash,
+//         builder: (context, state) => const SplashView(),
+//       ),
+
+//       /// Login
+//       GoRoute(
+//         path: RoutesName.login,
+//         name: RoutesName.login,
+//         builder: (context, state) => const LoginView(),
+//       ),
+
+//       /// Sign Up
+//       GoRoute(
+//         path: RoutesName.signUp,
+//         name: RoutesName.signUp,
+//         builder: (context, state) => const SignUpView(),
+//       ),
+
+//       /// Home
+//       GoRoute(
+//         path: RoutesName.home,
+//         name: RoutesName.home,
+//         builder: (context, state) => const HomeView(),
+//       ),
+//     ],
+//   );
+// }
---
+++ lib/core/utils/router/routes_name.dart
@@ -0,0 +1,13 @@
+class RoutesName {
+  RoutesName._();
+
+  /// Splash
+  static const String splash = '/';
+
+  /// Auth
+  static const String login = '/login';
+  static const String signUp = '/signup';
+
+  /// Home
+  static const String home = '/home';
+}
