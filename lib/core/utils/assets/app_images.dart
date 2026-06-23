ff7d57c40a8657954ac7bab687d9cd98f14e92aa
Nadia <nadiaashraff220@gmail.com>
2026-06-23 04:31:33 +0300

    chore: add AppImages class for centralized asset management

---
+++ lib/core/utils/assets/app_images.dart
@@ -0,0 +1,9 @@
+class AppImages {
+  AppImages._();
+
+  
+  static const String imagesSuccessDialog = 'assets/images/success_dialog.svg';
+  static const String imagesFailureDialog = 'assets/images/failure_dialog.svg';
+  static const String imagesSuccessSubmit = 'assets/images/success_submit.svg';
+  
+}
