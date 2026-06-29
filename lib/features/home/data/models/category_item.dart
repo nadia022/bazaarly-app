957763b4a60e1c263548306a749a380e29a17945
Nadia <nadiaashraff220@gmail.com>
2026-06-29 22:04:38 +0300

    refactor(home): remove unused CategoryItem model

--- lib/features/home/data/models/category_item.dart
+++
@@ -1,7 +0,0 @@
-/// Data model for a single category item
-class CategoryItem {
-  final String name;
-  final String imagePath;
-
-  const CategoryItem({required this.name, required this.imagePath});
-}
