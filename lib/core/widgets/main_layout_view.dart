7ba024d3005703b958a31bb55df1624cb2701584
Nadia <nadiaashraff220@gmail.com>
2026-07-01 17:30:58 +0300

    feat(cart): refactor CartCubit integration into MyApp and remove it from MainLayoutView

--- lib/core/widgets/main_layout_view.dart
+++ lib/core/widgets/main_layout_view.dart
@@ -1,10 +1,6 @@
-import 'package:bazarly_app/core/services/services_locator.dart';
 import 'package:bazarly_app/core/widgets/app_nav_bar.dart';
 import 'package:bazarly_app/core/widgets/main_layout_view_body.dart';
-import 'package:bazarly_app/features/cart/data/repos/cart_repo.dart';
-import 'package:bazarly_app/features/cart/presentation/view_models/cart_cubit/cart_cubit.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 /// The root scaffold of the app after login.
 ///
@@ -16,9 +12,6 @@ class MainLayoutView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => CartCubit(cartRepo: getIt<CartRepo>()),
-      child: const Scaffold(body: MainLayoutViewBody()),
-    );
+    return const Scaffold(body: MainLayoutViewBody());
   }
 }
--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,6 +1,10 @@
+import 'package:bazarly_app/core/services/services_locator.dart';
 import 'package:bazarly_app/core/utils/router/app_router.dart';
 import 'package:bazarly_app/core/utils/theme/app_theme.dart';
+import 'package:bazarly_app/features/cart/data/repos/cart_repo.dart';
+import 'package:bazarly_app/features/cart/presentation/view_models/cart_cubit/cart_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MyApp extends StatelessWidget {
@@ -13,27 +17,36 @@ class MyApp extends StatelessWidget {
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (context, child) {
-        return MaterialApp.router(
-          title: 'Bazaarly',
-          routerConfig: AppRouter.router,
-          theme: AppTheme.appTheme,
-          themeMode: ThemeMode.light,
-          debugShowCheckedModeBanner: false,
-
-          builder: (context, widget) {
-            final mediaQuery = MediaQuery.of(context);
-
-            return MediaQuery(
-              data: mediaQuery.copyWith(
-                textScaler: const TextScaler.linear(1.0),
-              ),
-              child: SafeArea(
-                top: false,
-                bottom: true,
-                child: widget ?? const SizedBox(),
-              ),
-            );
-          },
+        return MultiBlocProvider(
+          providers: [
+             BlocProvider<CartCubit>(
+          create: (_) => CartCubit(
+            cartRepo: getIt<CartRepo>(),
+          )..fetchUserCart(),
+        ),
+          ],
+          child: MaterialApp.router(
+            title: 'Bazaarly',
+            routerConfig: AppRouter.router,
+            theme: AppTheme.appTheme,
+            themeMode: ThemeMode.light,
+            debugShowCheckedModeBanner: false,
+          
+            builder: (context, widget) {
+              final mediaQuery = MediaQuery.of(context);
+          
+              return MediaQuery(
+                data: mediaQuery.copyWith(
+                  textScaler: const TextScaler.linear(1.0),
+                ),
+                child: SafeArea(
+                  top: false,
+                  bottom: true,
+                  child: widget ?? const SizedBox(),
+                ),
+              );
+            },
+          ),
         );
       },
     );
