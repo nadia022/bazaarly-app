6c81e18e7767bcad0ce43cbc7010eb5255914de0
Nadia <nadiaashraff220@gmail.com>
2026-07-01 15:59:02 +0300

    feat(cart): integrate CartCubit into AppSearchBar and MainLayoutView for cart management

--- lib/core/widgets/app_search_bar.dart
+++ lib/core/widgets/app_search_bar.dart
@@ -1,11 +1,28 @@
 import 'package:bazarly_app/core/utils/colors/app_colors.dart';
+import 'package:bazarly_app/core/utils/router/routes_name.dart';
 import 'package:bazarly_app/core/utils/styles/app_styles.dart';
+import 'package:bazarly_app/features/cart/presentation/view_models/cart_cubit/cart_cubit.dart';
+import 'package:bazarly_app/features/cart/presentation/view_models/cart_cubit/cart_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
 
 /// Search bar at the top of the home screen with a cart icon.
-class AppSearchBar extends StatelessWidget {
+class AppSearchBar extends StatefulWidget {
   const AppSearchBar({super.key});
+
+  @override
+  State<AppSearchBar> createState() => _AppSearchBarState();
+}
+
+class _AppSearchBarState extends State<AppSearchBar> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    context.read<CartCubit>().fetchUserCart();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,15 +62,51 @@ class AppSearchBar extends StatelessWidget {
           SizedBox(width: 12.w),
 
           // ── Cart icon ─────────────────────────────────────
-          GestureDetector(
-            onTap: () {
-              // TODO: navigate to cart screen
+          BlocBuilder<CartCubit, CartState>(
+            builder: (context, state) {
+              int count = 0;
+
+              if (state is FetchUserCartSuccess) {
+                count = state.response.numOfCartItems ?? 0;
+              }
+
+              return Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      context.push(RoutesName.cart);
+                    },
+                    child: Icon(
+                      Icons.shopping_cart_outlined,
+                      color: AppColors.primary,
+                      size: 28.sp,
+                    ),
+                  ),
+                  if (count > 0)
+                    Positioned(
+                      right: -6,
+                      top: -6,
+                      child: Container(
+                        width: 18.w,
+                        height: 18.w,
+                        decoration: const BoxDecoration(
+                          color: AppColors.primary,
+                          shape: BoxShape.circle,
+                        ),
+                        alignment: Alignment.center,
+                        child: Text(
+                          '$count',
+                          style: AppStyles.bodySmallRg.copyWith(
+                            color: Colors.white,
+                            fontSize: 12.sp,
+                          ),
+                        ),
+                      ),
+                    ),
+                ],
+              );
             },
-            child: Icon(
-              Icons.shopping_cart_outlined,
-              color: AppColors.primary,
-              size: 28.sp,
-            ),
           ),
         ],
       ),
--- lib/core/widgets/main_layout_view.dart
+++ lib/core/widgets/main_layout_view.dart
@@ -1,6 +1,10 @@
+import 'package:bazarly_app/core/services/services_locator.dart';
 import 'package:bazarly_app/core/widgets/app_nav_bar.dart';
 import 'package:bazarly_app/core/widgets/main_layout_view_body.dart';
+import 'package:bazarly_app/features/cart/data/repos/cart_repo.dart';
+import 'package:bazarly_app/features/cart/presentation/view_models/cart_cubit/cart_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 /// The root scaffold of the app after login.
 ///
@@ -12,6 +16,9 @@ class MainLayoutView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: MainLayoutViewBody());
+    return BlocProvider(
+      create: (context) => CartCubit(cartRepo: getIt<CartRepo>()),
+      child: const Scaffold(body: MainLayoutViewBody()),
+    );
   }
 }
